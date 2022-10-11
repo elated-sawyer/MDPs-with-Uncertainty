1274e66e8e7857a8fe022fdf5f3842546216406c
Siyu <siyu.zhou01@gmail.com>
2022-10-11 08:45:37 +0800

    delete RSMDPs

--- MOD.py
+++ MOD.py
@@ -24,61 +24,6 @@ def Dual(nS, nA, P_est, R_sa, gamma):
     #print("optimal solution", u_sol)
     return TAU, Policy
 
-def RSMDP(nS, nA, P_est, R_sa, TAU, Gamma):
-# Robust Satisficing MDP
-    # Parameters
-    D = np.ones(nS)*(1/nS)
-    W = np.ones(nS)*(1/nS)
-    W = W.transpose()
-    model = ro.Model()
-    # decision variable
-    mu = model.dvar((nS, nA))
-    k = model.dvar(nS)
-    zeta = model.dvar((nS, nS, nA, nS))
-    # random variables
-    p = model.rvar((nS, nA, nS))
-    # uncertainty set
-    Uset = (
-            p >= 0,     # Support Set 1
-            p.sum(axis=2) == 1,     # Support Set 2+
-            # rso.norm((p - P_emp).reshape(S*A*S), 1) <= 0.5     # Support Set 3+
-            )    # define the uncertainty set
-    # define model objective and constraints
-    model.minmax((W*k).sum(), Uset)
-    model.st( mu[s_prime, :].sum() - D[s_prime] - Gamma*(p[:, :, s_prime]*mu).sum() <= (zeta[s_prime, :, :, :] * (p - P_est)).sum() for s_prime in range(nS))
-    model.st(rso.norm((zeta[s_prime, :, :, :]).reshape(nS*nA*nS), 1) <= k[s_prime] for s_prime in range(nS))
-    model.st((R_sa * mu).sum() >= TAU)
-    model.st(k >= 0)
-    model.st(mu >= 0)
-    model.solve(msk)
-    mu_sol = mu.get()
-    # Calculate probs policy based on the solution mu
-    Probs = np.zeros([nS, nA])
-    for s in range(nS):
-        for a in range(nA):
-            if mu_sol.sum(axis=1)[s] != 0:
-                Probs[s, a] = np.divide(mu_sol[s, a], mu_sol.sum(axis=1)[s])
-            else:
-                Probs[s, a] = 1/nA
-            Probs = np.around(Probs, 4)
-    for s in range(nS):
-        if Probs.sum(axis=1)[s] != 1:
-            diff = 1 - Probs.sum(axis=1)[s]
-            index_NO = np.argsort(Probs[s])
-            if diff >= 0:
-                Probs[s, index_NO[-2]] += diff
-            else:
-                Probs[s, index_NO[-1]] += diff
-
-    TAU_real = ((R_sa * mu_sol).sum())
-    Policy = np.argmax(mu_sol, axis=1)
-    # print("mu optimal solution", mu_sol)
-    # print("optimal value", model.get())
-    # print("TAU is", TAU)
-    # print("TAU_real", TAU_real)
-    # print("optimal solution-probs", Probs)
-    # print("optimal policy is", Policy)
-    return TAU_real, Probs, Policy
 
 
 
--- README.md
+++ README.md
@@ -1,5 +1,19 @@
-# Robust Satisficing MDPs
+# Robust MDPs
+Despite being a fundamental building block for reinforcement learning, Markov decision processes (MDPs) often suffer from ambiguity in model parameters. Robust MDPs are proposed to overcome this challenge by optimizing the worst-case performance under ambiguity.<br>
+The performances of NMDPs, RMDPs, DRMDPs are evaluated in three applications: river swim, machine replacement and grid world.
 
+## Environments
+We use a discounted factor $\gamma=0.85$ for all environments, and the objective is always maximizing (total discounted) rewards.
+
+* Machine Replacement: we have 2 repair options constituting our action set ["repair", "do nothing"] and 10 states.
+The rewards relate only to the states, which are [20, 20, 20, 20, 20, 20, 20,  0, 18, 10].
+
+* River Swim: we have 2 swimming directions constituting our action set ["move left", "move right"] and 10 states, and the rewards relate only to the state, which are [5, 0, 0, 10, 10, 10, 10, 10, 10, 15]. 
+
+* Grid World: the grid world has two rows and 12 columns, and the rewards relate to the column indices only, which are [0, 3, 21, 27,  6,  0,  0,  0,  0,  0, 15, 24]. There are four available actions, "move up" and "move down" for vertical moves (that decreases and increases the column index, respectively), as well as "move left", and "move right" for horizontal moves (that decreases and increases the row index, respectively). Horizontal moves have a chance of failure that only related to row indices (0.9 for the first row and 0.2 for the second). Failing a transfer or selecting a vertical move would generate the column index of the next state according to a Dirichlet distribution. After selecting a horizontal move, the agent will randomly go up, go down, or stay 
+with probabilities $0.35$, $0.35$ and 0.3, respectively.
+
+# Files Intro
 ## Experiments Files
 **main.py**: conduct experiments on [Improvements on Percentile], save the estimated transition kernels in "Sampling", the results in "EXP_Results". <br>
 **ResultsPlots.ipynb**: plot the results obtained from "main" file.<br>
